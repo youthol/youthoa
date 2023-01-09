17daf233356367fd9af46b38b61561d02c4349ba
github-power <3352024270@qq.com>
2023-01-09 12:16:49 +0800

    feat: 配置国际化,使用中文
    antd 目前的默认文案是英文, 中文需要自行配置
    参考:https://3x.ant.design/docs/react/i18n-cn

--- src/index.js
+++ src/index.js
@@ -6,11 +6,19 @@ import App from '@/App';
 import '@/scss/reset.scss';
 import '@/scss/base.scss';
 import registerServiceWorker from '@/registerServiceWorker';
+// start 国际化-中文
+// antd 目前的默认文案是英文, 中文需要自行配置
+import zhCN from 'antd/es/locale/zh_CN';
+import { ConfigProvider } from 'antd';
+// end 国际化-中文
+
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <ConfigProvider locale={zhCN}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ConfigProvider>,
   document.getElementById('root')
 );
 registerServiceWorker();
